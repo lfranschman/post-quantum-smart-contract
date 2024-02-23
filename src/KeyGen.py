75e0f012cc3807cf61b8785e22e4b50182ade251
s3146480 <l.franschman@student.utwente.nl>
2024-02-23 16:23:30 +0100

    adding r1cs transform

--- src/KeyGen.py
+++ src/KeyGen.py
@@ -1,12 +1,11 @@
 import numpy as np
-import random
+
 n = 4
-k = 1
-q = 13
+q = 1666
 
 
 def mod_mult(m1, m2):
-    return (m1.dot(m2)) % q
+    return np.dot(m1, m2) % q
 
 def mod_add(m1, m2):
     return (m1 + m2) % q
@@ -21,10 +20,8 @@ def keyGen(s):
     t = mod_add(t, e)
     return a, t
 
-def encrypt(a, t, m):
+def encrypt(a, t, m, e1, e2):
     r = np.random.randint(3, size=n) - 1
-    e1 = np.random.randint(3, size=n) - 1
-    e2 = np.random.randint(3, size=1) - 1
     u = mod_mult(r, a)
     u = mod_add(u, e1)
     v = mod_mult(r, t)
@@ -35,24 +32,33 @@ def encrypt(a, t, m):
 def decrypt(s, u, v):
     f1 = mod_mult(u, s)
     m_with_error = mod_sub(v, f1)
-    return m_with_error
+    if (m_with_error[0] > (q//4)) and (m_with_error[0] < (3*(q//4))):
+        return 1
+    else:
+        return 0
 
 def main():
     s = np.random.randint(3, size=(n, 1)) - 1
     a, t = keyGen(s)
+    print("Public Key (a):")
     print(a)
+    print("\nSecret Key (t):")
     print(t)
     m = np.random.randint(2, size=1)
+    print("\nOriginal Message (m):")
     print(m)
-    u, v = encrypt(a, t, m)
-    me = decrypt(s, u, v)
-    if ((me[0] > (q/4)) & (me < (3*(q/4)))):
-        me[0] = 1
-    else:
-        me[0] = 0
-    print(me[0])
-    print(m[0])
-    print(me[0] == m[0])
+    e1 = np.random.randint(3, size=n) - 1
+    e2 = np.random.randint(3, size=1) - 1
+    u, v = encrypt(a, t, m, e1, e2)
+    m_to_check = decrypt(s, u, v)
+
+    # Correct the comparison conditions
+
+
+    print("\nDecrypted Message (me):")
+    print(m_to_check)
+    print("\nIs Decrypted Message equal to Original Message?")
+    print(m_to_check == m[0])
 
 if __name__ == "__main__":
     main()
