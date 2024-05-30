7a71fe067de6b7e1388dadc86181bc108f94f6fa
Lesley Franschman <l.franschman@student.tudelft.nl>
2024-05-31 01:58:25 +0200

    adding print for time results

--- src/fhelweSNARK.py
+++ src/fhelweSNARK.py
@@ -3,9 +3,7 @@ import numpy as np
 import fromLWEtoR1CS as r1
 import fromR1CStoQAP as qap
 import sys
-import time
-# import timeit
-
+import timeit
 
 order = 73
 GF = galois.GF(order)
@@ -13,56 +11,19 @@ q = 8929
 t = 73
 d = 57
 delta = q // t
-# Polynomial modulus
 p_q = np.poly1d([1] + ([0] * (d - 1)) + [1])
 
-
 def polymod(poly, poly_mod, coeff_mod):
-    """
-    Computes the remainder after a polynomial division
-    Args:
-        poly: Polynomial
-        poly_mod: Polynomial modulus
-        coeff_mod: Coefficient modulus
-    Returns:
-        The coefficients of the remainder when `poly` is divided by `poly_mod`
-    """
     return np.poly1d(np.floor(np.polydiv(poly, poly_mod)[1]) % coeff_mod)
 
-
 def addition(poly_mod, coeff_mod):
-    """
-    Creates a function which performs polynomial addition and auto-applys polynomial- and coefficient modulus
-    Args:
-        poly_mod: Polynomial modulus
-        coeff_mod: Coefficient modulus
-    Returns:
-        A function which takes polynomials `a` and `b` and adds them together
-    """
     return lambda a, b: np.poly1d(polymod(np.polyadd(a, b), poly_mod, coeff_mod))
 
-
 def multiplication(poly_mod, coeff_mod):
-    """
-    Creates a function which performs polynomial multiplication and auto-applys polynomial- and coefficient modulus
-    Args:
-        poly_mod: Polynomial modulus
-        coeff_mod: Coefficient modulus
-    Returns:
-        A function which takes polynomials `a` and `b` and multiplies them
-    """
     return lambda a, b: np.poly1d(polymod(np.polymul(a, b), poly_mod, coeff_mod))
 
 add = addition(p_q, q)
 mul = multiplication(p_q, q)
-
-
-def mod_horner(poly, x, modulus):
-    result = 0
-    for coefficient in poly.coefficients:
-        result = (result * x + coefficient) % modulus
-    return result
-
 
 def setup():
     alpha = np.random.randint(0, t, 1)[0]
@@ -79,67 +40,67 @@ def setup():
 
 def prover(pk, u, e1, alpha, s):
     A, B, C = r1.LWEToR1CS_transform()
-
     U, V, W, Ua, Va, Wa = qap.polySum(GF(s))
 
-    #vanishing polynomial
     T = galois.Poly([1, order - 1], field=GF)
     for i in range(2, len(A) + 1):
         T = T * galois.Poly([1, order - i], field=GF)
 
     H = (Ua * Va - Wa) // T
 
-    print("check if remainder is zero: ", (Ua * Va - Wa) % T)
-    # print("Ua: ", Ua)
-    # print("Va: ", Va)
-    # print("Wa: ", Wa)
-    # print("H: ", H)
-    # print("T: ", T)
-    #
-    # print("Uaalpha: ", Ua(alpha))
-    # print("Vaalpha: ", Va(alpha))
-    # print("Waalpha: ", Wa(alpha))
-    # print("Halpha: ", H(alpha))
-    # print("Talpha: ", T(alpha))
     left = Ua(alpha) * Va(alpha) - Wa(alpha)
     right = H(alpha) * T(alpha)
-    print("left: ", left)
-    print("right: ", right)
-    print("res1: ", left == right)
 
     c1 = add(add(mul(pk[0], u), e1), mul(delta, left))
-    print("c1 check:", c1)
     c2 = add(add(mul(pk[0], u), e1), mul(delta, right))
-    print("c2 check:", c2)
-    # e_enc = add(mul(pk[1], u), e2)
 
     return c1, c2
-
 
 def verifier(proof):
     c_0, c_1 = proof
     check = add(c_0, -c_1)
-    print("just a check: ", int(check.coefficients[0]))
-
     return int(check.coefficients[0]) == 0
 
 def main():
-    s = np.array([1, 2, 4, 1, 2, 3, 2, 1, 0, 1, 1, 1, 0, 3, 0, 1, 3])
+    s = np.array([1, 5, 8, 1, 8, 4, 7, 1, 0, 6, 4, 9, 1, 0, 8, 0, 1, 5, 0, 3, 2, 1, 0, 0, 1, 1, 1, 0, 1, 4, 0, 0, 0, 6, 0, 0, 1, 0, 0, 0, 1, 5, 0, 0, 2, 4, 4, 4, 6, 6, 7, 0, 0, 1, 5, 5, 7])
     alpha, sk, a2, e, pk, u, e1, e2 = setup()
-    print(alpha)
-    proof = prover(pk, u, e1, alpha, s)
-    start = time.time()
-    verification = verifier(proof)
 
-    end = time.time()
-    # execution_time = timeit.timeit('verifier()', globals=globals(), number=100)
-    # print("average execution time over 100 runs: ", execution_time)
+    start_prover = timeit.default_timer()
+    proof = prover(pk, u, e1, alpha, s)
+    end_prover = timeit.default_timer()
+
+    start_verifier = timeit.default_timer()
+    verification = verifier(proof)
+    end_verifier = timeit.default_timer()
+
     print("verification result:", verification)
-    print("verification time: ", end - start)
+    print("proof generation runtime: ", end_prover - start_prover, "s")
+    print("verification time: ", end_verifier - start_verifier, "s")
     print("proof size: ", sys.getsizeof(proof), " bytes")
 
+    return end_prover - start_prover, end_verifier - start_verifier
 
 if __name__ == "__main__":
     main()
-    # execution_time = timeit.timeit('main()', globals=globals(), number=100)
-    # print("average execution time over 100 runs: ", execution_time)
+    prover_times = []
+    verifier_times = []
+    for _ in range(50):
+        prover_time, verifier_time = main()
+        prover_times.append(prover_time)
+        verifier_times.append(verifier_time)
+
+    average_prover_time = sum(prover_times) / len(prover_times)
+    average_verifier_time = sum(verifier_times) / len(verifier_times)
+
+    min_prover_time = min(prover_times)
+    max_prover_time = max(prover_times)
+    min_verifier_time = min(verifier_times)
+    max_verifier_time = max(verifier_times)
+
+    print(f"Average proof generation time over 50 runs: {average_prover_time} s")
+    print(f"Lowest proof generation time over 50 runs: {min_prover_time} s")
+    print(f"Highest proof generation time over 50 runs: {max_prover_time} s")
+
+    print(f"Average verification time over 50 runs: {average_verifier_time} s")
+    print(f"Lowest verification time over 50 runs: {min_verifier_time} s")
+    print(f"Highest verification time over 50 runs: {max_verifier_time} s")
